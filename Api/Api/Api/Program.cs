6570aa7c99fda64ee0aea8ef0811c23f139db3e7
IverSande <iver.sande@aritma.com>
2025-03-24 19:05:09 +0100

    compose now works, but https need to be fixed, cant communicate between services, but they work individually

--- Api/Api/Api/Program.cs
+++ Api/Api/Api/Program.cs
@@ -2,8 +2,6 @@ using Api.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
@@ -15,7 +13,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 
 app.MapControllers();
